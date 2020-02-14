df68e5bee7ac9b6bc9aa31c384f06f64838b88a3
Ashish <ashish.kmr@utlook.com>
2020-02-15 02:12:40 +0530

    Now getting Headlines

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const cheerio = require('cheerio');
 const request = require('request');
 const userAgents = require('./userAgents');
 
+const headLines = []
+
 const options = {
     url: 'https://news.google.com/?hl=en-IN&gl=IN&ceid=IN:en',
     headers: {
@@ -19,13 +21,20 @@ function callback(error, response, body) {
 function extractInfo(info) {
     const $ = cheerio.load(info);
     const a = Array.from($('[jscontroller="d0DtYd"]'));
-    const b = Array.from($('.SbNwzf') )
-    console.log(b.length)
+    const b = Array.from($(a).find('.SbNwzf') )
     b.forEach((el, index)=>{
         if (index == 0){
             // console.log($($(el)).hasClass('.ipQwMb.ekueJc.RD0gLb').text());
             // console.log('Title :::::', $(el).find('.ipQwMb.ekueJc.RD0gLb').first().text());
-            console.log('Title :::::', $(el).find('.ipQwMb.ekueJc.RD0gLb').text());
+            c = Array.from($(el).children());
+            c.forEach((ele, index)=>{
+            //     console.log($(ele).find('h4').text());
+            //     console.log($(ele).find('.xBbh9').text());
+                console.log($(ele).find('.ipQwMb.ekueJc.RD0gLb').find('a').attr('href').replace('./','https://news.google.com/'));
+
+
+            })
+            // console.log('\n ************************************************ \n');
 
             // console.log('Image :::::', $(el).find('img').attr('src'))
             // console.log('sourceLink :::::', $(el).find('article').attr('jslog').match(/\bhttps?:\/\/\S+/gi)[0])
@@ -33,8 +42,13 @@ function extractInfo(info) {
             // console.log('Time :::::', $(el).find('time').attr('datetime'))
         }
     })
+    // extractHeadlines(a);
 
-    // console.log(a.length)
+    console.log(a.length)
+    console.log(b.length)
+
+    // console.log(c.length)
+
     
 }
 
@@ -45,6 +59,30 @@ function crawlNews() {
     } catch (error) {
         console.log(error);
     }
+}
+function extractHeadlines(rawData){
+    const $ = cheerio.load(rawData);
+    rawData.forEach((el, index)=>{
+            const id = index;
+            const newsTitle = $(el).find('.ipQwMb.ekueJc.RD0gLb').first().text();
+            const newsDesc = $(el).find('.xBbh9').first().text();
+            const thumbNail = $(el).find('img').attr('src');
+            const source = $(el).find('.wEwyrc.AVN2gc.uQIVzc').first().text();
+            const sourceRef = $(el).find('article').attr('jslog').match(/\bhttps?:\/\/\S+/gi)[0];
+            const timeStamp = $(el).find('time').attr('datetime');
+
+            headLines.push({
+                id,
+                newsTitle,
+                newsDesc,
+                thumbNail,
+                source,
+                sourceRef,
+                timeStamp
+            })
+    })
+    console.log(headLines)
+    
 }
 
 crawlNews();
