f906cabcc309e46340eca5a4b89f7572fe094ab9
Ashish <ashish.kmr@outlook.com>
2020-02-17 16:32:26 +0530

    Try Puppeteer instead of request

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
+//  TODO: Remove Request Completely
+// const request = require('request');
+// const userAgents = require('./userAgents');
+
 const cheerio = require('cheerio');
-const request = require('request');
+const puppeteer = require('puppeteer');
 const fs = require('fs')
-const userAgents = require('./userAgents');
+
 
 const headLines = []
 
-const options = {
-    url: 'https://news.google.com/?hl=en-IN&gl=IN&ceid=IN:en',
-    headers: {
-        'User-Agent': userAgents[Math.floor(Math.random() * userAgents.length)]
-    }
-};
+//  TODO: Remove Request Completely
+// const options = {
+//     url: 'https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFp0Y1RjU0FtVnVHZ0pKVGlnQVAB/sections/CAQiT0NCQVNOUW9JTDIwdk1EWnRjVGNTQldWdUxVZENHZ0pKVGlJUUNBUWFEQW9LTDIwdk1EUTJOak15Y3lvTUVnb3ZiUzh3TkRZMk16SnpLQUEqLggAKioICiIkQ0JBU0ZRb0lMMjB2TURadGNUY1NCV1Z1TFVkQ0dnSkpUaWdBUAFQAQ?hl=en-IN&gl=IN&ceid=IN%3Aen',
+//     headers: {
+//         'User-Agent': userAgents[Math.floor(Math.random() * userAgents.length)]
+//     }
+// };
 
 function callback(error, response, body) {
     if (!error && response.statusCode == 200) {
@@ -30,13 +35,25 @@ function extractInfo(info) {
     console.log(rawRelated.length)
         }
 
-function crawlNews() {
-    try {
-        request(options, callback);
-
-    } catch (error) {
-        console.log(error);
+async function crawlNews() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://news.google.com/?hl=en-IN&gl=IN&ceid=IN:en');
+    const info = await page.content();
+    if(info){
+        extractInfo(info)
     }
+
+    await browser.close();
+
+    //  TODO: Remove Request Completely
+
+        // try {
+        //     request(options, callback);
+
+        // } catch (error) {
+        //     console.log(error);
+        // }
 }
 
 function extractHeadlines(rawHeadlines){
