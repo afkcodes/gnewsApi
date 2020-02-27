014a93986942702fd1ccccba9414bf3fbe770936
Ashish Kumar <ashish.kmr@outlook.com>
2020-02-27 22:30:42 +0530

    Start serving news Data (WIP) (#5)
    * Start serving news Data (WIP)
    
    * remove package-lock.json & ignore

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser')
+const crawl = require('./app')
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+var requestTime = function (req, res, next) {
+    if(req.path === '/news'){
+        crawl('in', 'en');
+        next()
+    }
+  }
+  
+  
+  app.get('/', function (req, res) {
+    res.send('hello')
+})
+    
+  app.use(requestTime)
+  app.get('/news', async function (req, res) {
+    let news = await require('./news_data/Headlines.json')
+    // let news = JSON.parse('./news_data/Headlines.json');
+    res.json(news);
+  })
+  
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
